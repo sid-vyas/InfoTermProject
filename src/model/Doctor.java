a3290e2dbf06d72ef4a67d15c3052839b69e26d3
sid-vyas <vyas.si@northeastern.edu>
2023-03-26 15:47:48 -0400

    Made doctor directory and doctor class and patient login form.

---
+++ src/model/Doctor.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package model;
+
+public class Doctor {
+    String name;
+    String userId;
+    String hospitalName;
+    String community;
+    int age;
+    String specialization;
+    String gender;
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(String community) {
+        this.community = community;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/model/DoctorDirectory.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package model;
+
+import java.util.ArrayList;
+
+public class DoctorDirectory {
+    ArrayList<Doctor> allDoctors;
+    
+    public DoctorDirectory() {
+        this.allDoctors = new ArrayList<>();
+    }
+
+    public ArrayList<Doctor> getAllDoctors() {
+        return allDoctors;
+    }
+
+    public void setAllDoctors(ArrayList<Doctor> allDoctors) {
+        this.allDoctors = allDoctors;
+    }
+    
+    public void addDoctor(Doctor newDoctor) {
+        this.allDoctors.add(newDoctor);
+    }
+    
+    public void removeDoctor(Doctor doctor) {
+        this.allDoctors.remove(doctor);
+    }
+}
