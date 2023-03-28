2b474b2a63c8e05a7e13ad097e814fb5d5dd32d6
sid-vyas <vyas.si@northeastern.edu>
2023-03-28 01:09:47 -0400

    Made UI changes and made a Admin model file as well as an Admin login page

---
+++ src/model/Admin.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package model;
+
+public class Admin {
+    String userId;
+    String name;
+    
+    //levelOfAccess can be "System", "Hospital" and "Community"  
+    String levelOfAccess;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevelOfAccess() {
+        return levelOfAccess;
+    }
+
+    public void setLevelOfAccess(String levelOfAccess) {
+        this.levelOfAccess = levelOfAccess;
+    }
+}
